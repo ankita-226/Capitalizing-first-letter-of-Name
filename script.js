330502dbf11150c86b963539938b00c9455a6a67
Ankita Yadav <ankitayaduvanshi226@gmail.com>
2023-05-11 14:56:50 +0530

    second commit

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const result = document.getElementById("blankContainer")
 
 button.addEventListener("click",capitalize = () =>{
  event.preventDefault()
-    let firstLetter = userName.value
-    let modifiedName = firstLetter.charAt(0).toUpperCase()+firstLetter.slice(1)
+    let Username = userName.value
+    const words = Username.split(" ") ;
+    for(let i=0; i<words.length; i++){
+      words[i] = words[i].charAt(0).toUpperCase()+words[i].slice(1).toLowerCase()  
+    }
+    let modifiedName = words.join(" ")
  result.innerText =`Your good name is ${modifiedName}!`; 
 
 
